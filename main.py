99908915c285c5c2f76ff8e9eb0a0caa1763b942
caiden20000 <121005054+caiden20000@users.noreply.github.com>
2023-06-20 06:32:07 +0000

    wip is_move_legal

--- main.py
+++ main.py
@@ -47,15 +47,26 @@ def init_board(width: int = 8, height: int = 8):
 def _board_key(x: int, y: int) -> str:
     return str(x*10 + y)
 
+def validate_coords_string(coord: str) -> bool:
+    """Returns true if coord string is valid. EG. e1 is valid, w9 is not."""
+    if len(coord) != 2: return False
+    letter = "abcdefgh"
+    number = "12345678"
+    if coord[0] not in letter: return False
+    if coord[1] not in number: return False
+    return True
+
 def coords_stringify(x: int, y: int) -> str:
     """Returns a human readable coordinate, a1 to h8."""
     conv = "abcdefgh"
+    if out_of_bounds(x, y):
+        raise ValueError(f"Coordinates out of bounds! Given: ({x}, {y})")
     return conv[x] + str(y+1)
 
 def coords_string_to_tuple(coord: str) -> tuple:
     """Converts human readable coordinates (a1, g3, ...) to x, y tuple."""
-    if len(coord) != 2:
-        raise ValueError("Coordinate must be length of 2! Received: " + coord)
+    if not validate_coords_string(coord):
+        raise ValueError("The given coordinates are not valid! Given: " + coord)
     conv = "abcdefgh"
     return (conv.index(coord[0]), int(coord[1]))
 
@@ -117,7 +128,28 @@ def standard_board_setup():
     insert_piece(Piece(PieceType.KNIGHT, PieceColor.BLACK), 6, 7)
     insert_piece(Piece(PieceType.ROOK, PieceColor.BLACK), 7, 7)
 
+# Accepts input in xy-xy format. (source-destination)
+def parse_move(move: str) -> list[tuple]:
+    """Parses moves in 'xy-xy' format. Returns [(x,y), (x,y)]."""
+    if len(move) is not 5:
+        raise ValueError("Incorrect string length for move. Given: " + move)
+    coords = move.split("-")
+    if len(coords) != 2:
+        raise ValueError("Incorrectly delimited! Given: " + move)
+    if not validate_coords_string(coords[0]):
+        raise ValueError("First coordinate formatting error. Given: " + coords[0])
+    if not validate_coords_string(coords[1]):
+        raise ValueError("Second coordinate formatting error. Given: " + coords[1])
+    # String parsing done, convert coords for internal use
+    return [coords_string_to_tuple(coords[0]), coords_string_to_tuple(coords[1])]
+
+def is_move_legal(move: str) -> bool:
+    coords = parse_move(move)
 
 init_board()
 standard_board_setup()
 print(stringify_board())
+
+while True:
+    eto = input()
+    print(eto)
