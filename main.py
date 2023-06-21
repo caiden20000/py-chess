fcbba8bebf827a957eb86190d1042221280eb495
caiden20000 <121005054+caiden20000@users.noreply.github.com>
2023-06-21 07:08:57 +0000

    Convert to Coord object transactions

--- main.py
+++ main.py
@@ -62,8 +62,10 @@ class Coords:
         self.x = Coords.NUMBERS.index(string[0])
         self.y = Coords.LETTERS.index(string[1])
     def to_board_key(self):
-        return str(x*10 + y)
-        
+        return str(self.x*10 + self.y)
+
+# TODO: Add Coords object to piece
+# TODO: Change board from containing strings to containing pieces
 
 class Piece:
     """Class to represent a chess piece."""
@@ -82,10 +84,7 @@ def init_board(width: int = 8, height: int = 8):
     board = {}
     for x in range(width):
         for y in range(height):
-            insert_piece(None, x, y)
-
-def _board_key(coords: Coords) -> str:
-    return str(x*10 + y)
+            insert_piece(None, Coords(x, y))
 
 def get_piece(coords: Coords) -> Piece | None:
     """Returns the piece at a given coordinate. Returns None if no piece or out of bounds."""
@@ -107,11 +106,11 @@ def stringify_board(show_coords: bool = False) -> str:
     for y in range(HEIGHT):
         result += "|"
         for x in range(WIDTH):
-            piece = get_piece(x, y)
+            piece = get_piece(Coords(x, y))
             if piece is None:
                 result += "   "
             else:
-                result += get_piece(x, y).stringify()
+                result += get_piece(Coords(x, y)).stringify()
             result += "|"
         result += "\n" + row_sep
     return result
@@ -120,41 +119,41 @@ def standard_board_setup():
     """Sets up the standard chess game initial state."""
     # Reminder: (0, 0) is a1
     for x in range(WIDTH):
-        insert_piece(Piece(PieceType.PAWN, PieceColor.WHITE), x, 1)
-        insert_piece(Piece(PieceType.PAWN, PieceColor.BLACK), x, 6)
+        insert_piece(Piece(PieceType.PAWN, PieceColor.WHITE), Coords(x, 1))
+        insert_piece(Piece(PieceType.PAWN, PieceColor.BLACK), Coords(x, 6))
     # White side
-    insert_piece(Piece(PieceType.ROOK, PieceColor.WHITE), 0, 0)
-    insert_piece(Piece(PieceType.KNIGHT, PieceColor.WHITE), 1, 0)
-    insert_piece(Piece(PieceType.BISHOP, PieceColor.WHITE), 2, 0)
-    insert_piece(Piece(PieceType.QUEEN, PieceColor.WHITE), 3, 0)
-    insert_piece(Piece(PieceType.KING, PieceColor.WHITE), 4, 0)
-    insert_piece(Piece(PieceType.BISHOP, PieceColor.WHITE), 5, 0)
-    insert_piece(Piece(PieceType.KNIGHT, PieceColor.WHITE), 6, 0)
-    insert_piece(Piece(PieceType.ROOK, PieceColor.WHITE), 7, 0)
+    insert_piece(Piece(PieceType.ROOK, PieceColor.WHITE), Coords(0, 0))
+    insert_piece(Piece(PieceType.KNIGHT, PieceColor.WHITE), Coords(1, 0))
+    insert_piece(Piece(PieceType.BISHOP, PieceColor.WHITE), Coords(2, 0))
+    insert_piece(Piece(PieceType.QUEEN, PieceColor.WHITE), Coords(3, 0))
+    insert_piece(Piece(PieceType.KING, PieceColor.WHITE), Coords(4, 0))
+    insert_piece(Piece(PieceType.BISHOP, PieceColor.WHITE), Coords(5, 0))
+    insert_piece(Piece(PieceType.KNIGHT, PieceColor.WHITE), Coords(6, 0))
+    insert_piece(Piece(PieceType.ROOK, PieceColor.WHITE), Coords(7, 0))
     # Black side
-    insert_piece(Piece(PieceType.ROOK, PieceColor.BLACK), 0, 7)
-    insert_piece(Piece(PieceType.KNIGHT, PieceColor.BLACK), 1, 7)
-    insert_piece(Piece(PieceType.BISHOP, PieceColor.BLACK), 2, 7)
-    insert_piece(Piece(PieceType.QUEEN, PieceColor.BLACK), 3, 7)
-    insert_piece(Piece(PieceType.KING, PieceColor.BLACK), 4, 7)
-    insert_piece(Piece(PieceType.BISHOP, PieceColor.BLACK), 5, 7)
-    insert_piece(Piece(PieceType.KNIGHT, PieceColor.BLACK), 6, 7)
-    insert_piece(Piece(PieceType.ROOK, PieceColor.BLACK), 7, 7)
+    insert_piece(Piece(PieceType.ROOK, PieceColor.BLACK), Coords(0, 7))
+    insert_piece(Piece(PieceType.KNIGHT, PieceColor.BLACK), Coords(1, 7))
+    insert_piece(Piece(PieceType.BISHOP, PieceColor.BLACK), Coords(2, 7))
+    insert_piece(Piece(PieceType.QUEEN, PieceColor.BLACK), Coords(3, 7))
+    insert_piece(Piece(PieceType.KING, PieceColor.BLACK), Coords(4, 7))
+    insert_piece(Piece(PieceType.BISHOP, PieceColor.BLACK), Coords(5, 7))
+    insert_piece(Piece(PieceType.KNIGHT, PieceColor.BLACK), Coords(6, 7))
+    insert_piece(Piece(PieceType.ROOK, PieceColor.BLACK), Coords(7, 7))
 
 # Accepts input in xy-xy format. (source-destination)
-def parse_move(move: str) -> list[tuple]:
+def parse_move(move: str) -> list[Coords]:
     """Parses moves in 'xy-xy' format. Returns [(x,y), (x,y)]."""
     if len(move) is not 5:
         raise ValueError("Incorrect string length for move. Given: " + move)
     coords = move.split("-")
     if len(coords) != 2:
         raise ValueError("Incorrectly delimited! Given: " + move)
-    if not validate_coords_string(coords[0]):
+    if not Coords.validate_str(coords[0]):
         raise ValueError("First coordinate formatting error. Given: " + coords[0])
-    if not validate_coords_string(coords[1]):
+    if not Coords.validate_str(coords[1]):
         raise ValueError("Second coordinate formatting error. Given: " + coords[1])
     # String parsing done, convert coords for internal use
-    return [coords_string_to_tuple(coords[0]), coords_string_to_tuple(coords[1])]
+    return [Coords(string = coords[0]), Coords(string = coords[1])]
 
 def is_move_legal(move: str) -> bool:
     """Central logic for whether a requested move is played or not."""
