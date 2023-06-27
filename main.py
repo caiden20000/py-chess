a2447f8d0c669beb8c2c5060c96efbff9afef139
caiden20000 <121005054+caiden20000@users.noreply.github.com>
2023-06-27 06:39:22 +0000

    Outline game loop

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ from enum import Enum
 
 WIDTH = 8
 HEIGHT = 8
-# Represents an empty space on the board
-EMPTY_SPACE = "0"
-
-# Board is dict
-board = {}
 
 class PieceColor(Enum):
     """Enum to represent a chess piece color."""
@@ -26,6 +21,7 @@ class PieceType(Enum):
     KING = "k"
 
 class Coords:
+    """Object to represent a position on a chess board."""
     MIN_WIDTH = 0
     MAX_WIDTH = 7
     MIN_HEIGHT = 0
@@ -59,110 +55,115 @@ class Coords:
         return Coords.LETTERS[self.x] + Coords.NUMBERS[self.y]
     def set_by_string(self, string):
         self.validate_str(string, raise_err = True)
-        self.x = Coords.NUMBERS.index(string[0])
-        self.y = Coords.LETTERS.index(string[1])
+        self.x = Coords.LETTERS.index(string[0])
+        self.y = Coords.NUMBERS.index(string[1])
     def to_board_key(self):
-        return str(self.x*10 + self.y)
-
-# TODO: Add Coords object to piece
-# TODO: Change board from containing strings to containing pieces
+        return f"{self.x}:{self.y}"
 
 class Piece:
     """Class to represent a chess piece."""
-    def __init__(self, piece_type: PieceType, color: PieceColor):
+    def __init__(self, piece_type: PieceType, color: PieceColor, coords: Coords):
         self.type = piece_type
         self.color = color
+        self.coords = coords
     def stringify(self) -> str:
         """Returns a string representation of the piece. Should be str of length 3."""
         return self.color.value + self.type.value.upper() + self.color.value
 
-
-
-def init_board(width: int = 8, height: int = 8):
-    """Initialize the board as empty."""
-    global board
-    board = {}
-    for x in range(width):
-        for y in range(height):
-            insert_piece(None, Coords(x, y))
-
-def get_piece(coords: Coords) -> Piece | None:
-    """Returns the piece at a given coordinate. Returns None if no piece or out of bounds."""
-    if coords.out_of_bounds():
-        return None
-    return board[coords.to_board_key()]
-
-def insert_piece(piece: Piece | None, coords: Coords) -> bool:
-    """Insert a piece at the given coordinates. Returns true if successful."""
-    if coords.out_of_bounds():
-        return False
-    board[coords.to_board_key()] = piece
-    return True
-
-def stringify_board(show_coords: bool = False) -> str:
-    """Returns the string representation of the board. For UI usage."""
-    row_sep = "+---"*WIDTH + "+\n"
-    result = row_sep
-    for y in range(HEIGHT):
-        result += "|"
-        for x in range(WIDTH):
-            piece = get_piece(Coords(x, y))
-            if piece is None:
-                result += "   "
-            else:
-                result += get_piece(Coords(x, y)).stringify()
+class Board:
+    """Class to represent a chess board."""
+    def __init__(self):
+        self.pieces: dict[Piece | None] = {}
+    def get_piece(self, coords: Coords) -> Piece | None:
+        try:
+            return self.pieces[coords.to_board_key()]
+        except KeyError:
+            return None
+    def set_piece(self, piece: Piece):
+        self.pieces[piece.coords.to_board_key()] = piece
+    def remove_piece(self, coords: Coords):
+        self.pieces[coords] = None
+    def move_piece(self, piece: Piece, new_coords: Coords):
+        self.pieces[piece.coords.to_board_key()] = None
+        piece.coords = new_coords
+        self.set_piece(piece)
+    def move(self, old_coords: Coords, new_coords: Coords) -> bool:
+        piece = self.get_piece(old_coords)
+        if piece is None:
+            return False
+        self.move_piece(piece, new_coords)
+        return True
+    def stringify(self, show_coords: bool = False):
+        row_sep = "+---"*WIDTH + "+\n"
+        result = row_sep
+        for y in range(HEIGHT):
             result += "|"
-        result += "\n" + row_sep
-    return result
-
-def standard_board_setup():
-    """Sets up the standard chess game initial state."""
-    # Reminder: (0, 0) is a1
-    for x in range(WIDTH):
-        insert_piece(Piece(PieceType.PAWN, PieceColor.WHITE), Coords(x, 1))
-        insert_piece(Piece(PieceType.PAWN, PieceColor.BLACK), Coords(x, 6))
-    # White side
-    insert_piece(Piece(PieceType.ROOK, PieceColor.WHITE), Coords(0, 0))
-    insert_piece(Piece(PieceType.KNIGHT, PieceColor.WHITE), Coords(1, 0))
-    insert_piece(Piece(PieceType.BISHOP, PieceColor.WHITE), Coords(2, 0))
-    insert_piece(Piece(PieceType.QUEEN, PieceColor.WHITE), Coords(3, 0))
-    insert_piece(Piece(PieceType.KING, PieceColor.WHITE), Coords(4, 0))
-    insert_piece(Piece(PieceType.BISHOP, PieceColor.WHITE), Coords(5, 0))
-    insert_piece(Piece(PieceType.KNIGHT, PieceColor.WHITE), Coords(6, 0))
-    insert_piece(Piece(PieceType.ROOK, PieceColor.WHITE), Coords(7, 0))
-    # Black side
-    insert_piece(Piece(PieceType.ROOK, PieceColor.BLACK), Coords(0, 7))
-    insert_piece(Piece(PieceType.KNIGHT, PieceColor.BLACK), Coords(1, 7))
-    insert_piece(Piece(PieceType.BISHOP, PieceColor.BLACK), Coords(2, 7))
-    insert_piece(Piece(PieceType.QUEEN, PieceColor.BLACK), Coords(3, 7))
-    insert_piece(Piece(PieceType.KING, PieceColor.BLACK), Coords(4, 7))
-    insert_piece(Piece(PieceType.BISHOP, PieceColor.BLACK), Coords(5, 7))
-    insert_piece(Piece(PieceType.KNIGHT, PieceColor.BLACK), Coords(6, 7))
-    insert_piece(Piece(PieceType.ROOK, PieceColor.BLACK), Coords(7, 7))
+            for x in range(WIDTH):
+                piece = self.get_piece(Coords(x, 7-y))
+                if piece is None:
+                    result += "   "
+                else:
+                    result += self.get_piece(Coords(x, 7-y)).stringify()
+                result += "|"
+            result += "\n" + row_sep
+        return result
+    def standard_board_setup(self):
+        for x in range(WIDTH):
+            self.set_piece(Piece(PieceType.PAWN, PieceColor.WHITE, Coords(x, 1)))
+            self.set_piece(Piece(PieceType.PAWN, PieceColor.BLACK, Coords(x, 6)))
+        # White side
+        self.set_piece(Piece(PieceType.ROOK, PieceColor.WHITE, Coords(0, 0)))
+        self.set_piece(Piece(PieceType.KNIGHT, PieceColor.WHITE, Coords(1, 0)))
+        self.set_piece(Piece(PieceType.BISHOP, PieceColor.WHITE, Coords(2, 0)))
+        self.set_piece(Piece(PieceType.QUEEN, PieceColor.WHITE, Coords(3, 0)))
+        self.set_piece(Piece(PieceType.KING, PieceColor.WHITE, Coords(4, 0)))
+        self.set_piece(Piece(PieceType.BISHOP, PieceColor.WHITE, Coords(5, 0)))
+        self.set_piece(Piece(PieceType.KNIGHT, PieceColor.WHITE, Coords(6, 0)))
+        self.set_piece(Piece(PieceType.ROOK, PieceColor.WHITE, Coords(7, 0)))
+        # Black side
+        self.set_piece(Piece(PieceType.ROOK, PieceColor.BLACK, Coords(0, 7)))
+        self.set_piece(Piece(PieceType.KNIGHT, PieceColor.BLACK, Coords(1, 7)))
+        self.set_piece(Piece(PieceType.BISHOP, PieceColor.BLACK, Coords(2, 7)))
+        self.set_piece(Piece(PieceType.QUEEN, PieceColor.BLACK, Coords(3, 7)))
+        self.set_piece(Piece(PieceType.KING, PieceColor.BLACK, Coords(4, 7)))
+        self.set_piece(Piece(PieceType.BISHOP, PieceColor.BLACK, Coords(5, 7)))
+        self.set_piece(Piece(PieceType.KNIGHT, PieceColor.BLACK, Coords(6, 7)))
+        self.set_piece(Piece(PieceType.ROOK, PieceColor.BLACK, Coords(7, 7)))
 
 # Accepts input in xy-xy format. (source-destination)
-def parse_move(move: str) -> list[Coords]:
-    """Parses moves in 'xy-xy' format. Returns [(x,y), (x,y)]."""
-    if len(move) is not 5:
-        raise ValueError("Incorrect string length for move. Given: " + move)
-    coords = move.split("-")
+def parse_move(move_str: str) -> tuple[Coords]:
+    """Parses moves in 'xy-xy' format. Returns tuple pair of Coords."""
+    if len(move_str) != 5:
+        raise ValueError("Incorrect string length for move. Given: " + move_str)
+    coords = move_str.split("-")
     if len(coords) != 2:
-        raise ValueError("Incorrectly delimited! Given: " + move)
+        raise ValueError("Incorrectly delimited! Given: " + move_str)
     if not Coords.validate_str(coords[0]):
         raise ValueError("First coordinate formatting error. Given: " + coords[0])
     if not Coords.validate_str(coords[1]):
         raise ValueError("Second coordinate formatting error. Given: " + coords[1])
     # String parsing done, convert coords for internal use
-    return [Coords(string = coords[0]), Coords(string = coords[1])]
+    return (Coords(string = coords[0]), Coords(string = coords[1]))
 
-def is_move_legal(move: str) -> bool:
+def is_move_legal(old_coords: Coords, new_coords: Coords) -> bool:
     """Central logic for whether a requested move is played or not."""
-    coords = parse_move(move)
+    return True
 
-init_board()
-standard_board_setup()
-print(stringify_board())
+def move(board: Board, move_str: str):
+    coords = parse_move(move_str)
+    if is_move_legal(*coords):
+        board.move(*coords)
+
+
+global_board = Board()
+global_board.standard_board_setup()
+print(global_board.stringify())
 
 while True:
-    eto = input()
-    print(eto)
+    user_input = input()
+    try:
+        move(global_board, user_input)
+    except KeyError:
+        print("Input error, try again:\n")
+    else:
+        print("\n\n" + global_board.stringify() + "\n")
